8559e01c0ec5cc38db85293b02a9dd83e7956d3a
Arthur Gonçalves <51856574+SeedPluss@users.noreply.github.com>
2020-05-31 20:44:58 -0300

    functions

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 function GerarNumero() {
+    var elemento_pai = document.body;
+    var valor = document.getElementById("valoraposta").value
+    var numeros = document.getElementById("numerosaposta").value
+
+    if (numero == ""){
+        alert("Insira o total de número que você quer apostar")
+        return
+    }
+    if (valor == ""){
+        alert("Insira o valor da aposta!")
+        return
+    }
     
 }
